b90caf6ea41f5015c30e1324d8339bec2c36647d
rain8841 <agekor@yandex.ru>
2016-04-13 10:48:21 +0300

    Fix Pack

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,9 +1,9 @@
 class Page < ActiveRecord::Base
-  @keywords_regex = /^([a-z]+)(,\s*[a-z]+)*$/i
+  @keywords_regex = /^([a-z]{1,25})(,\s*[a-z]{1,25})*$/i
   
-  validates :name, presence: true, length: {minimum: 2, maximum: 25}
-  validates :short_name, presence: true, length: {minimum: 2, maximum: 10}
-  validates :title, presence: true, uniqueness: true ,length: {minimum: 2, maximum: 25}
+  validates :name, presence: true, length: { in: 3..25 }
+  validates :short_name, presence: true, length: { in: 2..10 }
+  validates :title, presence: true, uniqueness: true ,length: { in: 3..25 }
   validates :meta_keywords, presence: true, allow_blank: true, format: {:with => @keywords_regex, :message => ': ' + I18n.t('invalKeyWords'), :multiline => true}
   validates :meta_description, presence: false
   validates :index, presence: true, uniqueness: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
